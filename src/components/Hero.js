8fec5d928f93347bfd445b9cd565987543fc8d87
hiromero <hiroshiromero0@gmail.com>
2022-10-21 10:38:17 +0800

    deploy website

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src="../images/photo-grid.png" className="hero--photo" alt="Hero"/>
+            <h1 className="hero--header">Join the Crew</h1>
+            <p className="hero--text">Join unique interactive activities led by 
+            one-of-a-kind the Humming Bird Crew without getting bored.</p>
+        </section>
+    )
+}
